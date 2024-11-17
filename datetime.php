4de6448600bfb0ca3fb3dd5882523d7a4805346a
dflfo <166027618+dflfo@users.noreply.github.com>
2024-11-17 20:26:04 -0300

    Update datetime.php

--- datetime.php
+++ datetime.php
@@ -1,6 +1,17 @@
 <?php
-date_default_timezone_set('America/Auncion_City');
-$fecha_actual(('Y-m-d H:i:s');
+header('Content-Type: application/json');
 
+// Establece la zona horaria a Paraguay
+date_default_timezone_set('America/Asuncion');
 
+// Obtiene la hora actual
+$hora_actual = date('Y-m-d H:i:s');
+
+// Crea un array asociativo para almacenar la respuesta
+$respuesta = array(
+    'datetime' => $hora_actual
+);
+
+// Convierte el array a formato JSON y lo imprime
+echo json_encode($respuesta);
 ?>
